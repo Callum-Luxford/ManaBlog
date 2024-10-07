03486fdff8e1eb0e9025dfa9047af490ae865597
Callum Luxford <151152271+Callum-Luxford@users.noreply.github.com>
2024-10-07 13:40:07 +0100

    Additional CRUD operations

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const expressLayout = require("express-ejs-layouts");
 
 const connectDB = require("./server/config/db");
 const { connect } = require("mongoose");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,6 +17,18 @@ connectDB();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser());
+
+app.use(
+  session({
+    secret: "Yamahaxsr700@",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI
+    }),
+  })
+);
 
 app.use(express.static("public"));
 
@@ -23,6 +38,7 @@ app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
 
 app.use("/", require("./server/routes/main"));
+app.use("/", require("./server/routes/admin"));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
